c111074cbbb144e7799f56d73d9ade4043044541
mbeka02 <antonymbeka@gmail.com>
2024-05-07 20:54:58 +0300

    added shadcn

--- certify/src/app/_components/modal-button.tsx
+++ certify/src/app/_components/modal-button.tsx
@@ -1,12 +1,10 @@
 "use client";
 
+import { Button } from "@/components/ui/button";
 import { useWeb3Modal } from "@web3modal/wagmi/react";
 
-
-
 export const ModalButton = () => {
-
   const { open } = useWeb3Modal();
 
-  return <button onClick={() => open()} className="flex items-center justify-center font-semibold text-white bg-white p-2 h-10 ">Open Modal</button>
-}
+  return <Button onClick={() => open()}>Connect Wallet</Button>;
+};
--- certify/src/app/page.tsx
+++ certify/src/app/page.tsx
@@ -2,8 +2,8 @@ import { ModalButton } from "./_components/modal-button";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="flex w-full items-center justify-center p-3 px-4">
+    <main className="flex min-h-screen flex-col">
+      <div className="flex w-full items-center justify-end p-3 px-4">
         <ModalButton />
       </div>
     </main>
