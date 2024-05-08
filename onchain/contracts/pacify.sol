e44ca64711a3f199aa03fadc43341a351fab84b2
PatrickOjiambo <pashrick237@students.uonbi.ac.ke>
2024-05-08 16:02:27 +0300

    test: writing tests for the smart contract

--- onchain/contracts/pacify.sol
+++ onchain/contracts/pacify.sol
@@ -4,7 +4,7 @@ pragma solidity ^0.8.24;
 
 import {ERC721URIStorage} from "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
-
+import "hardhat/console.sol";
 contract Pacific is ERC721URIStorage {
     uint256 private _nextTokenId;
 
@@ -15,19 +15,23 @@ contract Pacific is ERC721URIStorage {
         address recipient,
         string memory tokenURI
     ) public returns (uint256) {
+        // console.log("%s, %s", recipient, tokenURI);
         uint256 tokenId = _nextTokenId++;
         _mint(recipient, tokenId);
         _setTokenURI(tokenId, tokenURI);
-
+        // console.log("Token ID incoming", tokenId);
         return tokenId;
     }
 
-    //This function is fo transferring the certificate to another address
+    //This function is for transferring the certificate to another address
     function transferCert(address from, address to, uint256 tokenId) public {
         safeTransferFrom(from, to, tokenId);
     }
 
     function searchCert(uint256 tokenId) public view returns (string memory) {
         return tokenURI(tokenId);
+    }
+    function totalSupply() public view returns (uint256) {
+        return _nextTokenId;
     }
 }
--- onchain/test/nft.ts
+++ onchain/test/nft.ts
@@ -2,11 +2,49 @@ const { expect } = require("chai");
 
 import { ethers } from "hardhat";
 describe("NFT smart contract", function(){
-  it("Trying to deploy the smart contract first", async function(){
-    const [owner] = await ethers.getSigners();
-    const hardhatNFT = await ethers.deployContract("Pacific");
-    const minted_nft = await hardhatNFT.mintCert(owner.address, "https://game.example/item-id-8u5h2m.json");
+  let certNFTCollection: any
+  beforeEach(async ()=>{
     
-    console.log("Token ID:", minted_nft);
+    const hardhatNFT = await ethers.getContractFactory("Pacific");
+    certNFTCollection = await hardhatNFT.deploy();
   })
+  it("should mint NFT with tokenID 0", async function(){
+    const [owner] = await ethers.getSigners();
+    await certNFTCollection.mintCert(owner.address, "https://game.example/item-id-8u5h2m.json");
+    await certNFTCollection.mintCert(owner.address, "https://game.example/item-id-8u5h2m.json");
+    const totalSupply = await certNFTCollection.totalSupply();
+    expect(totalSupply).to.equal(2);
+  })
+  it("should return the tokenURI of the NFT", async function(){
+    const [owner] = await ethers.getSigners();
+    await certNFTCollection.mintCert(owner.address, "https://game.example/item-id-8u5h2m.json");
+    const tokenURI = await certNFTCollection.tokenURI(0);
+    expect(tokenURI).to.equal("https://game.example/item-id-8u5h2m.json");
+  }
+  )
+  it("should return the owner of the NFT", async function(){
+    const [owner] = await ethers.getSigners();
+    await certNFTCollection.mintCert(owner.address, "https://game.example/item-id-8u5h2m.json");
+    const ownerOfNFT = await certNFTCollection.ownerOf(0);
+    expect(ownerOfNFT).to.equal(owner.address);
+  })
+
 })
+
+
+
+// describe("NFT smart contract", function(){
+//   it("Trying to deploy the smart contract first", async function(){
+//     const [owner] = await ethers.getSigners();
+//     const hardhatNFT = await ethers.getContractFactory("Pacific");
+//     let HardhatNFT = await hardhatNFT.deploy();
+//     await HardhatNFT.deployed();
+
+//     const minted_nft = await hardhatNFT.mintCert(owner.address, "https://game.example/item-id-8u5h2m.json");
+//     // const searchedNft = await hardhatNFT.searchCert(minted_nft);
+//     const second_minted_nft = await hardhatNFT.mintCert(owner.address, "https://game.example/item-id-8u5h2m.json");
+//     console.log("Token ID:", minted_nft);
+//     // console.log("Token ID:", searchedNft);
+//     console.log("Token ID:", second_minted_nft); 
+//   })
+// })
