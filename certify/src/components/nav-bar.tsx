7836226c7a578f97ae27abed3498a5bcded6244e
mbeka02 <antonymbeka@gmail.com>
2024-05-08 19:36:02 +0300

    ui fixes

--- certify/src/components/nav-bar.tsx
+++ certify/src/components/nav-bar.tsx
@@ -1,7 +1,7 @@
-"use client"
+"use client";
 import { Button } from "@/components/ui/button";
 import Link from "next/link";
-import Image from 'next/image';
+import Image from "next/image";
 import { ModalButton } from "./modal-button";
 import TestTransaction from "@/app/test";
 
@@ -10,11 +10,12 @@ export default function NavBar() {
     <>
       <div className="w-full bg-black fixed top-0 left-0 right-0">
         <div className="justify-between px-4 py-1 mx-auto lg:max-w-7xl md:items-center md:flex md:px-8">
-
           {/* Left Section (PACIFY and Logo) */}
           <div className="flex items-center">
             <Link href="/">
-            <h2 className="text-2xl text-amber-500 font-mono font-bold mr-2 ml-10">PACIFY</h2>
+              <h2 className="text-2xl text-amber-500 font-mono font-bold mr-2 ml-10">
+                CERTIFY
+              </h2>
             </Link>
             <Image
               src="/gold.png"
