7fc5484f58abf746042b483d4f7cedc820a126f8
mbeka02 <antonymbeka@gmail.com>
2024-05-08 18:21:22 +0300

    added wallet address display

--- certify/src/components/modal-button.tsx
+++ certify/src/components/modal-button.tsx
@@ -1,13 +1,18 @@
 "use client";
 
 import { Button } from "@/components/ui/button";
+import { TruncateMiddle } from "@/config/format";
 import { useWeb3Modal } from "@web3modal/wagmi/react";
 import { useAccount } from "wagmi";
 
 export const ModalButton = () => {
   const { open } = useWeb3Modal();
-  const {address} = useAccount();
+  const { address, isConnected } = useAccount();
 
   console.log("Connected wallet address is =>", address);
-  return <Button onClick={() => open()}>Connect Wallet</Button>;
+  return (
+    <Button onClick={() => open()} disabled={isConnected}>
+      {address && isConnected ? TruncateMiddle(address, 6) : "connect wallet"}
+    </Button>
+  );
 };
