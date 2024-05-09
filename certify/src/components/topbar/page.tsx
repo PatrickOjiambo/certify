610cb6e3913a7edb594d0d0371d19f49a162b77e
mbeka02 <antonymbeka@gmail.com>
2024-05-09 17:34:22 +0300

    css fixes

--- certify/src/components/topbar/page.tsx
+++ certify/src/components/topbar/page.tsx
@@ -2,23 +2,16 @@
 "use client";
 import clsx from "clsx";
 import { LogOut } from "lucide-react";
-//import { signOut, useSession } from "next-auth/react";
 import React, { useEffect, useState } from "react";
-// import {AppContext} from '@/app/app-context'
-// import { truncateString } from '@/app/helpers/truncate_string'
-//import { LocalStorageKeys } from '@/app/helpers/local_storage_keys'
 import { useRouter } from "next/navigation";
 import BackButton from "../back-button";
 
 function DashboardTopBar() {
-  //const session = useSession()
   const router = useRouter();
-  //const app_context = useContext(AppContext);
-  //const address = localStorage.getItem(LocalStorageKeys.USER_ADDRESS);
   return (
     <div
       className={clsx(
-        "flex flex-row items-center justify-between w-11/12 border-b-[1px] border-b-neutral-50 rounded-lg  px-4 py-6 bg-black text-white mt-24 mb-2",
+        "flex flex-row items-center justify-between w-full border-b-[1px] border-b-neutral-50 rounded-sm  px-4 py-6 bg-black text-white mt-24 mb-2",
       )}
     >
       <BackButton />
